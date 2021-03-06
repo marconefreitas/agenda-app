b5ff7909e4ad38f02295931d9de1d00d0aaf73ff
Marcone Freitas <marcfreitas0@gmail.com>
2021-03-06 14:56:50 -0300

    Ultimo commit da agenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import {  MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule} from '@angular/material/dialog';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import {ReactiveFormsModule } from '@angular/forms'
+
 import { ContatoService } from './contato.service';
 import { ContatoComponent } from './contato/contato.component';
+import { ContatoDetalheComponent } from './contato-detalhe/contato-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContatoComponent
+    ContatoComponent,
+    ContatoDetalheComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+
+    HttpClientModule,
+
+    ReactiveFormsModule
   ],
   providers: [ContatoService],
   bootstrap: [AppComponent]
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ContatoService } from '../contato.service';
+import { Contato } from './contato';
+import {ContatoDetalheComponent} from '../contato-detalhe/contato-detalhe.component';
+
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog'
+import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contato',
@@ -7,10 +15,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  nome: string = "Marcone";
-  constructor() { }
+  formulario : FormGroup;
+  contatos : Contato[] = [];
+  colunas = ['foto', 'id', 'nome', 'email', 'favorito'];
+  totalElementos : number  = 0;
+  pagina :number = 0;
+  tamanho :number = 2;
+  pageSizeOptions :number[]   = [10];
+
+
+  constructor(private service : ContatoService, 
+              private fb : FormBuilder,
+              private dialog : MatDialog,
+              private snackBar : MatSnackBar) {
+   
+   }
 
   ngOnInit(): void {
+    this.montarFormulario();
+    this.listarContatos(this.pagina, this.tamanho);
+  }
+
+  favoritar(contato : Contato){
+    this.service.favorite(contato).subscribe(response => {
+      contato.favorito = !contato.favorito;
+    });
+  }
+  
+  visualizarContato(contato : Contato){
+
+    this.dialog.open(ContatoDetalheComponent, {
+      width : '400px',
+      height : '450px',
+      data : contato
+    } )
+  }
+
+  montarFormulario(){
+    this.formulario = this.fb.group({
+      nome : ['', Validators.required],
+      email: ['', Validators.email]
+    });
+  }
+
+  listarContatos(pagina, tamanho){
+    this.service.list(pagina, tamanho).subscribe(response => {
+      this.contatos = response.content;
+      this.totalElementos = response.totalElements;
+      this.pagina = response.number
+    })
+  }
+
+  submit(){
+    let formValues = this.formulario.value;
+    let cont : Contato = new Contato(formValues.nome, formValues.email);
+    this.service
+    .salvar(cont)
+    .subscribe(response => {
+     
+      this.listarContatos(this.pagina, this.tamanho);
+      this.snackBar.open('Contato adicionado', 'Sucesso', {duration : 2000});
+      this.formulario.reset();
+    });
+   
+  }
+  paginar(event : PageEvent){
+    this.pagina = event.pageIndex;
+    this.listarContatos(this.pagina, this.tamanho);
+  }
+
+  upload(event, contato){
+    let files = event.target.files;
+
+    if (files){
+      let foto = files[0];
+      let formData = new FormData();
+      formData.append("foto", foto);
+      this.service
+      .upload(contato, formData)
+      .subscribe(response => {
+        this.listarContatos(this.pagina, this.tamanho);
+      });
+    }
   }
 
 }
